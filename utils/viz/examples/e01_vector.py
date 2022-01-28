83d9b6d0a9780a83e0aa1e80c4d8ccc4a99c969c
muhammad-yazdian <muhammad.yazdian@gmail.com>
2022-01-28 18:48:01 +0100

    Added Vector3d examples

--- utils/viz/examples/e01_vector.py
+++ utils/viz/examples/e01_vector.py
@@ -1,27 +1,28 @@
-#=======================================================
+#=========================================================
 # e01_vector.py
 # 
-# A simple example for drawing vectors using graphiclib
+# A simple example for drawing 3D vectors using graphiclib
 #
 # Seied Muhammad Yazdian | Jan 28, 2022
-#======================================================
+#=========================================================
   
 import numpy as np
 import matplotlib.pyplot as plt
 import graphiclib_path
 import graphiclib as gl
 
-# Create some points
-a = gl.Point(0, 0, 0)
-b = gl.Point(3, 0, 0)
-c = gl.Point(3, 3, 0)
-d = gl.Point(3, 3, 1)
+# Create some 3D points
+point_a = gl.Point3d(0, 0, 0)
+point_b = gl.Point3d(3, 0, 0)
+point_c = gl.Point3d(0, 2, 0)
+point_d = gl.Point3d(3, 3, 1)
 
-# Create some arrows/vectors based on the available points
-arrow_1 = gl.Arrow(a, b, 'k')
-arrow_2 = gl.Arrow(b, c, 'g')
-arrow_3 = gl.Arrow(a, c, 'b')
-arrow_tool = gl.Arrow(b, d, 'r')
+# Create some 3D vectors
+vector_a = gl.Vector3d(0, 0, 0)
+vector_b = gl.Vector3d(3, 0, 0)
+vector_c = gl.Vector3d(0, 2, 0)
+vector_d = gl.Vector3d(3, 3, 1)
+vector_e = vector_b + vector_c
 
 # Display the arrows
 fig = plt.figure()
@@ -30,8 +31,12 @@ plt.xlim(-1, 4)
 plt.ylim(-1, 4)
 # plt.zlim(-3, 3) # Not supported.
 ax.set_aspect('auto', 'box') # 'equal is not supported'
-arrow_1.draw(ax)
-arrow_2.draw(ax)
-arrow_3.draw(ax)
-arrow_tool.draw(ax)
+
+# Create and display some arrows/vectors based on the available points
+arrow_1 = gl.Arrow3D2(point_a, point_b, ax, 'k')
+arrow_2 = gl.Arrow3D2(point_b, point_c, ax, 'g')
+arrow_3 = gl.Arrow3D2(point_a, point_c, ax, 'b')
+arrow_4 = gl.Arrow3D2(point_b, point_d, ax, 'r')
+arrow_5 = gl.Arrow3D2(point_a, vector_e, ax, 'r')
+
 plt.show()
