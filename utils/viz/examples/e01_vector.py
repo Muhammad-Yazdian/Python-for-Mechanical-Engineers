7f2569180035f7f5bc00452e20a37b1a3764caad
muhammad-yazdian <muhammad.yazdian@gmail.com>
2022-01-29 12:31:24 +0100

    Added a cross product example

--- utils/viz/examples/e01_vector.py
+++ utils/viz/examples/e01_vector.py
@@ -5,7 +5,7 @@
 #
 # Seied Muhammad Yazdian | Jan 28, 2022
 #=========================================================
-  
+
 import numpy as np
 import matplotlib.pyplot as plt
 import graphiclib_path
@@ -23,6 +23,9 @@ vector_b = gl.Vector3d(3, 0, 0)
 vector_c = gl.Vector3d(0, 2, 0)
 vector_d = gl.Vector3d(3, 3, 1)
 vector_e = vector_b + vector_c
+vector_f = vector_b.cross(vector_c)
+print(type(vector_e))
+print(type(vector_f))
 
 # Display the arrows
 fig = plt.figure()
@@ -32,11 +35,12 @@ plt.ylim(-1, 4)
 # plt.zlim(-3, 3) # Not supported.
 ax.set_aspect('auto', 'box') # 'equal is not supported'
 
-# Create and display some arrows/vectors based on the available points
-arrow_1 = gl.Arrow3D2(point_a, point_b, ax, 'k')
-arrow_2 = gl.Arrow3D2(point_b, point_c, ax, 'g')
-arrow_3 = gl.Arrow3D2(point_a, point_c, ax, 'b')
-arrow_4 = gl.Arrow3D2(point_b, point_d, ax, 'r')
-arrow_5 = gl.Arrow3D2(point_a, vector_e, ax, 'r')
+# Create and display some arrows/vectors based on the available points/vectors
+arrow_a = gl.Arrow3D2(point_a, point_b, ax, 'k')
+arrow_b = gl.Arrow3D2(point_b, point_c, ax, 'g')
+arrow_c = gl.Arrow3D2(point_a, point_c, ax, 'b')
+arrow_d = gl.Arrow3D2(point_b, point_d, ax, 'r')
+arrow_e = gl.Arrow3D2(point_a, vector_e, ax, 'r')
+arrow_f = gl.Arrow3D2(point_a, vector_f, ax, 'b')
 
 plt.show()
