3947604717a931b5ed613570471d2a972775699e
muhammad-yazdian <muhammad.yazdian@gmail.com>
2022-01-30 12:37:51 +0100

    Added draw contents

--- Robotics/examples/e01_rotate_frame.py
+++ Robotics/examples/e01_rotate_frame.py
@@ -1,11 +1,24 @@
-import sys
+# import sys
 import roboticlib_path
 import roboticlib as rl
 
 theta = float(0)
-a = rl.rotatoinMatixX(theta)
-print('Rx = \n', a)
+rot_mat_1 = rl.rotatoinMatixZ(theta)
+print('Rx = \n', rot_mat_1)
 
-theta = float(30)
-a = rl.rotatoinMatixX(theta)
-print('Rx = \n', a)
+theta = float(10)
+rot_mat_2 = rl.rotatoinMatixZ(theta)
+print('Rx = \n', rot_mat_2)
+
+# Display contents
+import numpy as np
+import matplotlib.pyplot as plt 
+import graphiclib_path
+import graphiclib as gl
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+gl.draw(ax, 'frame', rot_mat_1, position=np.array([0,0,0]))
+gl.draw(ax, 'frame', rot_mat_2, position=np.array([0,0,0]))
+gl.set_axes_equal(ax)
+plt.show()
