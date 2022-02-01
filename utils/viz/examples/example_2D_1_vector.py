f8d5fb74e2649c97fa7360d9f9a12c38c26024bb
muhammad-yazdian <muhammad.yazdian@gmail.com>
2022-02-01 18:41:17 +0100

    Added an example using list class

--- utils/viz/examples/example_2D_1_vector.py
+++ utils/viz/examples/example_2D_1_vector.py
@@ -1,15 +1,15 @@
 #=========================================================
 # example_2D_1_vector.py
-# 
+#
 # A simple example for drawing 2D vectors using graphiclib
 #
 # Author(s):
 #   Seied Muhammad Yazdian
-# 
+#
 # Last update:
 #   Feb 1, 2022
 #=========================================================
-  
+
 import numpy as np
 import matplotlib.pyplot as plt
 import graphiclib_path
@@ -24,6 +24,7 @@ vector_1 = np.array([1, 1, 0])
 # Display contents
 fig = plt.figure()
 ax = plt.axes()
+gl.drawArrow(ax, [0, 0], [1, 0.5])
 gl.drawArrow(ax, point_0, point_1, color='r')
 gl.drawArrow(ax, point_0, point_0 + vector_1, color='g')
 ax.set_aspect('equal')
