68ec400e84beca3e2bbd1f9731af37a38d442e49
muhammad-yazdian <muhammad.yazdian@gmail.com>
2022-02-01 19:27:08 +0100

    Fixed bugs

--- utils/viz/examples/example_2D_1_vector.py
+++ utils/viz/examples/example_2D_1_vector.py
@@ -17,15 +17,16 @@ import graphiclib as gl
 import mathlib_path
 import mathlib as ml
 
-point_0 = np.array([0, 0, 0])
-point_1 = np.array([2, 0, 0])
-vector_1 = np.array([1, 1, 0])
+point_0 = np.array([1, 0, 0])
+point_1 = np.array([2, 0.1, 0])
+vector_1 = np.array([1.5, 1, 0])
 
 # Display contents
 fig = plt.figure()
 ax = plt.axes()
-gl.drawArrow(ax, [0, 0], [1, 0.5])
-gl.drawArrow(ax, point_0, point_1, color='r')
-gl.drawArrow(ax, point_0, point_0 + vector_1, color='g')
+gl.drawArrow(ax, [1, 1], [0, 0])
+gl.drawArrow(ax, [1, 1.1], [0, 0], color='r')
+gl.drawArrow(ax, vector_1, point_0, color='g')
+gl.drawArrow(ax, vector_1, point_1, color='b')
 ax.set_aspect('equal')
 plt.show()
